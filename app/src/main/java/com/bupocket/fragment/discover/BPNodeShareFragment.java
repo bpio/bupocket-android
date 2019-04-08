a93ca2f0a0aa2c4ce9cf0d712c86fb286db7dbd0
zhangxinars <zhangxinars@163.com>
2019-04-08 14:54:18 +0800

    share

---
+++ app/src/main/java/com/bupocket/fragment/discover/BPNodeShareFragment.java
@@ -0,0 +1,44 @@
+package com.bupocket.fragment.discover;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.bupocket.R;
+import com.bupocket.base.BaseFragment;
+import com.qmuiteam.qmui.widget.QMUITopBarLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BPNodeShareFragment extends BaseFragment {
+    @BindView(R.id.topbar)
+    QMUITopBarLayout mTopBar;
+
+    @Override
+    protected View onCreateView() {
+        View root = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_node_plan, null);
+        ButterKnife.bind(this, root);
+        init();
+        return root;
+    }
+
+    private void init() {
+        initUI();
+    }
+
+    private void initUI() {
+        initTopBar();
+    }
+
+    private void initTopBar() {
+        mTopBar.setBackgroundDividerEnabled(false);
+        mTopBar.addLeftImageButton(R.mipmap.icon_tobar_left_arrow, R.id.topbar_left_arrow).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popBackStack();
+            }
+        });
+        mTopBar.setTitle(getResources().getString(R.string.invite_share_txt));
+    }
+}
