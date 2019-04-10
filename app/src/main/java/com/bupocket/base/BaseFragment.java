b8dbf2e938097e6df0156557fb6f9bb75d6eed46
guohuan1990 <liuchuanguo111@163.com>
2019-04-10 18:25:34 +0800

    baseFragment Class add  address method

--- app/src/main/java/com/bupocket/base/BaseFragment.java
+++ app/src/main/java/com/bupocket/base/BaseFragment.java
@@ -3,7 +3,10 @@ package com.bupocket.base;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 
+import com.bupocket.utils.CommonUtil;
+import com.bupocket.utils.LogUtils;
 import com.bupocket.utils.SharedPreferencesHelper;
 import com.qmuiteam.qmui.arch.QMUIFragment;
 import com.qmuiteam.qmui.util.QMUIDisplayHelper;
@@ -38,6 +41,40 @@ public abstract class BaseFragment extends QMUIFragment {
         super.onResume();
 
 //        BPUpgradeManager.getInstance(getContext()).runUpgradeTipTaskIfExist(getActivity());
+    }
+
+
+    public String getWalletAddress(){
+       String currentIdentityWalletAddress = spHelper.getSharedPreference("currentWalletAddress","").toString();
+        if(TextUtils.isEmpty(currentIdentityWalletAddress)) {
+            return spHelper.getSharedPreference("currentAccAddr", "").toString();
+        }
+        return currentIdentityWalletAddress;
+    }
+
+    /**
+     *
+     * @return   false currentAccAddr  true currentWalletAddress
+     */
+    private boolean isCurrentAddress(){
+        String currentIdentityWalletAddress = spHelper.getSharedPreference("currentWalletAddress","").toString();
+        if (TextUtils.isEmpty(currentIdentityWalletAddress)) {
+            return false;
+        }
+        String currentAccAddr = spHelper.getSharedPreference("currentAccAddr", "").toString();
+        if (currentAccAddr.equals(currentIdentityWalletAddress)){
+            return false;
+        }
+        return true;
+    }
+
+
+    public String getBPAccountData(){
+        String  accountBPData = spHelper.getSharedPreference("BPData", "").toString();
+        if (isCurrentAddress()){
+            return  spHelper.getSharedPreference( spHelper.getSharedPreference("currentWalletAddress","").toString()+ "-BPdata", "").toString();
+        }
+        return accountBPData;
     }
 
 }
