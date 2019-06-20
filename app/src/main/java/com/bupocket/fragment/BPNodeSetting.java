dab86e5f6b306f48f6debe37480cf05d547037d1
guohuan1990 <liuchuanguo111@163.com>
2019-06-20 10:54:54 +0800

    node setting

--- app/src/main/java/com/bupocket/fragment/BPNodeSetting.java
+++ app/src/main/java/com/bupocket/fragment/BPNodeSetting.java
@@ -2,12 +2,14 @@ package com.bupocket.fragment;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import com.bupocket.BPApplication;
 import com.bupocket.R;
@@ -27,17 +29,12 @@ import butterknife.Unbinder;
 
 public class BPNodeSetting extends AbsBaseFragment {
 
-    @BindView(R.id.ivNodeSetMain)
-    ImageView ivNodeSetMain;
-    @BindView(R.id.llNodeSettingMain)
-    LinearLayout llNodeSettingMain;
-    @BindView(R.id.ivNodeSetTestMain)
-    ImageView ivNodeSetTestMain;
-    @BindView(R.id.llNodeSettingTestMain)
-    LinearLayout llNodeSettingTestMain;
     @BindView(R.id.topbar)
     QMUITopBarLayout topbar;
-    Unbinder unbinder;
+    @BindView(R.id.lvNodeSet)
+    ListView lvNodeSet;
+    @BindView(R.id.llAddMore)
+    LinearLayout llAddMore;
 
 
     @Override
@@ -52,13 +49,15 @@ public class BPNodeSetting extends AbsBaseFragment {
 
     @Override
     protected void initData() {
-        if(SharedPreferencesHelper.getInstance().getInt("bumoNode", Constants.DEFAULT_BUMO_NODE) == BumoNodeEnum.TEST.getCode()){
-            ivNodeSetTestMain.setVisibility(View.VISIBLE);
-            ivNodeSetMain.setVisibility(View.INVISIBLE);
-        }else {
-            ivNodeSetTestMain.setVisibility(View.INVISIBLE);
-            ivNodeSetMain.setVisibility(View.VISIBLE);
-        }
+
+
+//        if (SharedPreferencesHelper.getInstance().getInt("bumoNode", Constants.DEFAULT_BUMO_NODE) == BumoNodeEnum.TEST.getCode()) {
+//            ivNodeSetTestMain.setVisibility(View.VISIBLE);
+//            ivNodeSetMain.setVisibility(View.INVISIBLE);
+//        } else {
+//            ivNodeSetTestMain.setVisibility(View.INVISIBLE);
+//            ivNodeSetMain.setVisibility(View.VISIBLE);
+//        }
     }
 
     @Override
@@ -75,21 +74,21 @@ public class BPNodeSetting extends AbsBaseFragment {
 
             }
         });
-        topbar.setTitle(R.string.node_setting_title);
 
-        Button rightButton = topbar.addRightTextButton(getString(R.string.save), R.id.topbar_right_button_save);
+        Button rightButton = topbar.addRightTextButton(getString(R.string.save), R.id.skipBackupBtn);
         rightButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean isChecked = ivNodeSetMain.getVisibility() == View.VISIBLE;
-                if(isChecked){
-                    showSwitchMainNetDialog();
-                }else {
-                    ShowSwitchTestNetConfirmDialog();
-                }
+//                boolean isChecked = ivNodeSetMain.getVisibility() == View.VISIBLE;
+//                if (isChecked) {
+//                    showSwitchMainNetDialog();
+//                } else {
+//                    ShowSwitchTestNetConfirmDialog();
+//                }
             }
         });
-        rightButton.setTextColor(R.color.app_color_green);
+        Button skipBackuoBtn = topbar.findViewById(R.id.skipBackupBtn);
+        skipBackuoBtn.setTextColor(ContextCompat.getColor(getContext(),R.color.app_color_main));
 
     }
 
@@ -102,8 +101,8 @@ public class BPNodeSetting extends AbsBaseFragment {
                         SharedPreferencesHelper.getInstance().save("bumoNode", BumoNodeEnum.TEST.getCode());
                         BPApplication.switchNetConfig(BumoNodeEnum.TEST.getName());
                         dialog.dismiss();
-                        spHelper.put("tokensInfoCache","");
-                        spHelper.put("tokenBalance","");
+                        spHelper.put("tokensInfoCache", "");
+                        spHelper.put("tokenBalance", "");
                         startFragment(new HomeFragment());
                     }
                 }).setCanceledOnTouchOutside(false).create().show();
@@ -119,31 +118,10 @@ public class BPNodeSetting extends AbsBaseFragment {
                         dialog.dismiss();
                         SharedPreferencesHelper.getInstance().save("bumoNode", BumoNodeEnum.MAIN.getCode());
                         BPApplication.switchNetConfig(BumoNodeEnum.MAIN.getName());
-                        spHelper.put("tokensInfoCache","");
-                        spHelper.put("tokenBalance","");
+                        spHelper.put("tokensInfoCache", "");
+                        spHelper.put("tokenBalance", "");
                         startFragment(new HomeFragment());
                     }
                 }).setCanceledOnTouchOutside(false).create().show();
-    }
-
-
-    @OnClick({R.id.llNodeSettingMain, R.id.llNodeSettingTestMain})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.llNodeSettingMain:
-            case R.id.llNodeSettingTestMain:
-                switch (ivNodeSetMain.getVisibility()) {
-                    case View.VISIBLE:
-                        ivNodeSetMain.setVisibility(View.INVISIBLE);
-                        ivNodeSetTestMain.setVisibility(View.VISIBLE);
-                        break;
-                    case View.INVISIBLE:
-                        ivNodeSetMain.setVisibility(View.VISIBLE);
-                        ivNodeSetTestMain.setVisibility(View.INVISIBLE);
-                        break;
-                }
-
-                break;
-        }
     }
 }
