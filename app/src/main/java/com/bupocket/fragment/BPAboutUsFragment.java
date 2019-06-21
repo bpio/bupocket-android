17463d39d9ecc7bc825fb891bacbe60375093fe2
guohuan1990 <liuchuanguo111@163.com>
2019-06-21 09:21:54 +0800

    about us

--- app/src/main/java/com/bupocket/fragment/BPAboutUsFragment.java
+++ app/src/main/java/com/bupocket/fragment/BPAboutUsFragment.java
@@ -1,9 +1,43 @@
 package com.bupocket.fragment;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.bupocket.R;
 import com.bupocket.base.AbsBaseFragment;
+import com.bupocket.common.Constants;
+import com.bupocket.utils.CommonUtil;
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
+import com.qmuiteam.qmui.widget.QMUITopBarLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class BPAboutUsFragment extends AbsBaseFragment {
+    @BindView(R.id.topbar)
+    QMUITopBarLayout topBar;
+    @BindView(R.id.versionCodeTV)
+    TextView versionCodeTV;
+    @BindView(R.id.versionInfoListLL)
+    LinearLayout versionInfoListLL;
+    @BindView(R.id.tvProfileLanguage)
+    TextView tvProfileLanguage;
+    @BindView(R.id.addressBookRL)
+    LinearLayout addressBookRL;
+    @BindView(R.id.changeTestLL)
+    LinearLayout changeTestLL;
+    @BindView(R.id.changeTestTV)
+    TextView changeTestTV;
+    @BindView(R.id.customEnvironmentLL)
+    LinearLayout customEnvironmentLL;
+    Unbinder unbinder;
+
     @Override
     protected int getLayoutView() {
         return R.layout.fragment_about_us;
@@ -11,7 +45,18 @@ public class BPAboutUsFragment extends AbsBaseFragment {
 
     @Override
     protected void initView() {
+        initTopBar();
+        versionCodeTV.setText(getString(R.string.about_us_version_code).concat(CommonUtil.packageName(mContext)));
+    }
 
+    private void initTopBar() {
+        topBar.setTitle(R.string.profile_about_us);
+        topBar.addLeftImageButton(R.mipmap.icon_tobar_left_arrow, R.id.topbar_left_arrow).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popBackStack();
+            }
+        });
     }
 
     @Override
@@ -23,4 +68,5 @@ public class BPAboutUsFragment extends AbsBaseFragment {
     protected void setListeners() {
 
     }
+
 }
