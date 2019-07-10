4c1f9fef7b3b16ba74b0780df744c9f2479cc4ea
liuchuanguo <liuchuanguo111@163.com>
2019-07-10 14:41:26 +0800

    backup  view

--- app/src/main/java/com/bupocket/fragment/BPAssetsHomeFragment.java
+++ app/src/main/java/com/bupocket/fragment/BPAssetsHomeFragment.java
@@ -114,9 +114,8 @@ public class BPAssetsHomeFragment extends BaseFragment {
     @BindView(R.id.addTokenLl)
     LinearLayout mAddTokenLl;
     @BindView(R.id.immediatelyBackupBtn)
-    QMUIRoundButton mImmediatelyBackupBtn;
-    @BindView(R.id.notBackupBtn)
-    QMUIRoundButton mNotBackupBtn;
+    Button mImmediatelyBackupBtn;
+
     @BindView(R.id.safetyTipsLl)
     LinearLayout mSafetyTipsLl;
     @BindView(R.id.manageWalletBtn)
@@ -227,13 +226,7 @@ public class BPAssetsHomeFragment extends BaseFragment {
                 startFragment(bpUserInfoFragment);
             }
         });
-        mNotBackupBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mSafetyTipsLl.setVisibility(View.GONE);
-                sharedPreferencesHelper.put("backupTipsState", BackupTipsStateEnum.HIDE.getCode());
-            }
-        });
+
         mManageWalletBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
