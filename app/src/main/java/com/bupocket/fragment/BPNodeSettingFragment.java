d4a48a1f83b851df8f88fd9eefb525b52e29feb1
liuchuanguo <liuchuanguo111@163.com>
2019-07-22 17:05:21 +0800

    change wallet head icon update view

--- app/src/main/java/com/bupocket/fragment/BPNodeSettingFragment.java
+++ app/src/main/java/com/bupocket/fragment/BPNodeSettingFragment.java
@@ -178,6 +178,7 @@ public class BPNodeSettingFragment extends AbsBaseFragment {
     @SuppressLint("ResourceAsColor")
     private void initTopBar() {
         topbar.setBackgroundDividerEnabled(false);
+        topbar.setTitle(R.string.node_setting_title);
         topbar.addLeftImageButton(R.mipmap.icon_tobar_left_arrow, R.id.topbar_left_arrow).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
