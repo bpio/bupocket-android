b2e06ac8de448c13b7157f653d15437588fa645f
liuchuanguo <liuchuanguo111@163.com>
2019-08-03 14:08:17 +0800

    open red packet view

--- app/src/main/java/com/bupocket/activity/RedPacketActivity.java
+++ app/src/main/java/com/bupocket/activity/RedPacketActivity.java
@@ -2,11 +2,15 @@ package com.bupocket.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.bupocket.R;
 import com.bupocket.utils.ShareUtils;
+import com.makeramen.roundedimageview.RoundedImageView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,9 +21,31 @@ public class RedPacketActivity extends Activity {
 
 
     @BindView(R.id.redPacketDetailLL)
-    LinearLayout redPacketLL;
+    LinearLayout redPacketDetailLL;
     @BindView(R.id.saveShareBtn)
     Button saveShareBtn;
+    @BindView(R.id.openRedPacketBtn)
+    Button openRedPacketBtn;
+    @BindView(R.id.closeRedBtn)
+    ImageView closeRedBtn;
+    @BindView(R.id.redPacketFirstLL)
+    LinearLayout redPacketFirstLL;
+    @BindView(R.id.redAmountTv)
+    TextView redAmountTv;
+    @BindView(R.id.downloadQrIv)
+    ImageView downloadQrIv;
+    @BindView(R.id.redTopIv)
+    ImageView redTopIv;
+    @BindView(R.id.cancelRedDetailBtn)
+    ImageView cancelRedDetailBtn;
+    @BindView(R.id.redHeadIv)
+    RoundedImageView redHeadIv;
+    @BindView(R.id.redNameIv)
+    TextView redNameIv;
+    @BindView(R.id.redTokenTypeTv)
+    TextView redTokenTypeTv;
+    @BindView(R.id.redWalletAddressTv)
+    TextView redWalletAddressTv;
 
     private Unbinder bind;
 
@@ -37,24 +63,35 @@ public class RedPacketActivity extends Activity {
     }
 
     private void initView() {
-
+        redPacketDetailLL.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
+//        super.onBackPressed();
     }
 
 
-    @OnClick(R.id.saveShareBtn)
-    public void onViewClicked() {
-        shareRedPacket();
+    @OnClick({R.id.openRedPacketBtn, R.id.closeRedBtn, R.id.saveShareBtn, R.id.cancelRedDetailBtn})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.openRedPacketBtn:
+                redPacketFirstLL.setVisibility(View.GONE);
+                redPacketDetailLL.setVisibility(View.VISIBLE);
+                break;
+            case R.id.closeRedBtn:
+            case R.id.cancelRedDetailBtn:
+                finish();
+                break;
+            case R.id.saveShareBtn:
+                shareRedPacket();
+                break;
+        }
     }
+
 
     private void shareRedPacket() {
-        ShareUtils.shareImage(redPacketLL, this);
-//        ShareUtils.saveImage("redPacket",redPacketLL,this);
+        ShareUtils.shareImage(redPacketDetailLL, this);
+//        ShareUtils.saveImage("redPacket",redPacketDetailLL,this);
     }
-
-
 }
